075103b3f3a73c06afe257bdcbf1cd8b634e8386
lolavilardell03 <lolavilardell03@gmail.com>
2026-04-06 13:21:44 +0200

    app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Configuramos la pestaña del navegador
+st.set_page_config(page_title="Reloj Ayurvédico", page_icon="🧘‍♂️", layout="centered")
+
+# Nuestro primer título
+st.title("🕰️ Mi Reloj Ayurvédico")
+st.write("¡El esqueleto de la aplicación funciona perfectamente!")
